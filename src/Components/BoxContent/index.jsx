6c8fe24f253b1c360e10a7c72788b0baf03a19bc
svandu <svandu08@gmail.com>
2023-07-06 16:27:57 +0530

    box content and cards component updated

--- src/Components/BoxContent/index.jsx
+++ src/Components/BoxContent/index.jsx
@@ -1,7 +1,40 @@
-function BoxContent() {
+import { useEffect } from "react";
+import axios from "axios";
+
+import "./boxcontent.css";
+
+// eslint-disable-next-line react/prop-types
+function BoxContent({show, setShow, setCategories, category}) {
+  useEffect(() => {
+    const fetchApi = async () => {
+      try{
+        const response = await axios.get('https://api.chucknorris.io/jokes/random?category={category}');  
+        setCategories(response.data);
+      } catch (error) {
+        console.log('Error fetching categories:', error);
+      }
+    };
+
+    fetchApi();
+  },[]);
+ 
+  const closeBox = () => {
+    setShow(!show);
+  }
   return (
-    <div>BoxContent</div>
-  )
+    <div className="box-main-container">
+      <div className="head">
+        <p className="box-head">{category}</p>
+        <button className="cross-icon" onClick={closeBox}>&times;</button>
+      </div>
+      <div className="box-para">
+        <p className="paragraph">
+          {category.value}
+        </p>
+        <button className="next-btn">Next Joke</button>
+      </div>
+    </div>
+  );
 }
 
-export default BoxContent
+export default BoxContent;
--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import BoxContent from "../BoxContent";
+import { Link } from "react-router-dom";
 import "./cards.css"
 
 function Cards() {
   const [categories, setCategories] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
+  const [show, setShow] = useState(false);
 
-  const toggleBox = () => {
-    setIsOpen(!isOpen);
+  const showBox = () => {
+    setShow(!show);
   }
 
   useEffect(() => {
     const fetchApi = async () => {
       try{
-        const response = await axios.get('https://api.chucknorris.io/jokes/categories');
+        const response = await axios.get('https://api.chucknorris.io/jokes/categories');  
         setCategories(response.data);
       } catch (error) {
         console.log('Error fetching categories:', error);
@@ -25,16 +26,21 @@ function Cards() {
   }, []);
 
   return (
-    <div>
+    <div className="cards-main-container">
       <span className="card-heading">Chuck Norries</span>
-      <div className="card-main-container">
+      <Link to={`/categories/${categories.id}`} className="link-tag">
+      <div className="card-container">
           {categories.map((category) => (
-            <div key={category} className="card-container" onClick={toggleBox}>{category}
+            <div key={category} className="single-card-container" onClick={showBox}>{category}
               <p className="para">Unlimited Jokes on {category}</p>
+
+              { 
+                show && <BoxContent className="box-container" category= {category} setShow={setShow} show={show} setCategories = {setCategories}/> 
+              }
             </div>
           ))}
       </div>
-      <BoxContent />
+      </Link>
     </div>
   )
 }
